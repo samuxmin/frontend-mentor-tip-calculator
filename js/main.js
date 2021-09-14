d77d2964a727071a4bf6f0025a261619e9d528a7
Samuel Mindler <mindlersamuel@gmail.com>
2021-09-14 12:38:37 -0300

    remove reset bug solution 2

--- js/main.js
+++ js/main.js
@@ -77,6 +77,6 @@ function handleReset () {
     custom_tip.value = ""
     persons.value = 0
     bill.value = 0
+    toggleActiveButton({target:buttons.children[2]})
     handleError('remove')
-    toggleActiveButton(document.querySelectorAll('.btn')[0])
 }
